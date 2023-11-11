76e5793657699492f809f7c61931d8ac489afdf3
RaspberryPiEdge <raspberrypi.edge@gmail.com>
2023-11-11 17:51:00 -0500

    Update webservice-test.py

--- Chapter2/code/webservice-test.py
+++ Chapter2/code/webservice-test.py
@@ -1,14 +1,21 @@
 import requests
 from sense_hat import SenseHat
 
-response = requests.get('https://jsonplaceholder.typicode.com/posts')
+response = requests.get(
+    'https://jsonplaceholder.typicode.com/posts'
+)
 sense = SenseHat()
 sense.set_rotation(270)
 
 if response.status_code == 200:
     data = response.json()
     print(data[0]['title'])
-    sense.show_message(f'Success with code: {response.status_code}')
+    
+    success_msg = 'Success with code: '
+    success_msg += str(response.status_code)
+    sense.show_message(success_msg)
 else:
-    print(f'Failed with status code: {response.status_code}')
-    sense.show_message(f'Failed with code: {response.status_code}')
+    error_msg = 'Failed with code: '
+    error_msg += str(response.status_code)
+    print(error_msg)
+    sense.show_message(error_msg)
