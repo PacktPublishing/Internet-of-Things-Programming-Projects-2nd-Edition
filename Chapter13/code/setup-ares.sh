f129c92faf53395e633d0f24eff7fada2f60a872
RaspberryPiEdge <raspberrypi.edge@gmail.com>
2024-03-11 01:17:33 -0400

    Update setup-ares.sh

--- Chapter13/code/setup-ares.sh
+++ Chapter13/code/setup-ares.sh
@@ -14,7 +14,7 @@ apt-get update && apt-get upgrade -y
 
 # Install necessary system utilities
 echo "Installing necessary system utilities..."
-apt-get install -y python3-pip python3-smbus i2c-tools git libi2c-dev
+apt-get install -y python3-pip python3-smbus i2c-tools git
 
 # Enable I2C and UART
 echo "Enabling I2C and UART interfaces..."
@@ -22,33 +22,31 @@ raspi-config nonint do_i2c 0
 raspi-config nonint do_serial 0
 
 # Install Adafruit Blinka for CircuitPython libraries support
-echo "Installing Adafruit Blinka and other Python libraries for hardware interfacing..."
+echo "Installing Adafruit Blinka and other Python libraries..."
 pip3 install --upgrade pip
 pip3 install Adafruit-Blinka adafruit-circuitpython-vl53l0x pyserial RPi.GPIO
 
-# Install ROS 2 Humble Hawksbill
-echo "Setting up ROS 2 Humble Hawksbill environment..."
-# Add the ROS 2 apt repository
+# Install ROS Humble Hawksbill
+echo "Installing ROS Humble Hawksbill and required packages..."
 apt update && apt install -y curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | apt-key add -
-echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/ros2.list
-
-# Install ROS 2 Humble Hawksbill
+echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/ros2.list
 apt update
-apt install -y ros-humble-desktop
+apt install -y ros-humble-ros-base
+
+# Install Colcon build system
+echo "Installing Colcon build system..."
+apt install -y python3-colcon-common-extensions
 
 # Initialize rosdep
+echo "Initializing rosdep..."
 rosdep init
 rosdep update
 
 # Environment setup
+echo "Adding ROS 2 environment setup to your bashrc..."
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-# Install additional ROS 2 utilities
-apt install -y python3-colcon-common-extensions python3-rosdep
-
-# Update pip and setuptools
-pip3 install -U pip setuptools
-
-echo "Setup complete! Please restart."
+# Setup complete
+echo "Setup complete! Your Raspberry Pi is now ready for I2C, UART projects, GPIO manipulation with CircuitPython, and ROS Humble development."
