e7b38154ea9c6eb6128249089c24f7ee4261e2f8
RaspberryPiEdge <raspberrypi.edge@gmail.com>
2024-03-11 01:14:55 -0400

    Update setup-ares.sh

--- Chapter13/code/setup-ares.sh
+++ Chapter13/code/setup-ares.sh
@@ -6,38 +6,49 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-echo "Starting setup..."
+echo "Beginning setup..."
 
 # Update and upgrade the system
 echo "Updating and upgrading your system. This might take a while."
 apt-get update && apt-get upgrade -y
 
-# Install Python 3, PIP, and other essential packages
-echo "Installing Python 3, PIP, and necessary packages..."
-apt-get install -y python3-pip python3-smbus i2c-tools git
+# Install necessary system utilities
+echo "Installing necessary system utilities..."
+apt-get install -y python3-pip python3-smbus i2c-tools git libi2c-dev
 
-# Enable I2C interface
-echo "Enabling the I2C interface..."
+# Enable I2C and UART
+echo "Enabling I2C and UART interfaces..."
 raspi-config nonint do_i2c 0
-
-# Enable UART interface
-echo "Enabling the UART interface and disabling the serial console..."
 raspi-config nonint do_serial 0
 
-# Install Adafruit Blinka (CircuitPython library support on Raspberry Pi)
-echo "Installing Adafruit Blinka (CircuitPython support)..."
+# Install Adafruit Blinka for CircuitPython libraries support
+echo "Installing Adafruit Blinka and other Python libraries for hardware interfacing..."
 pip3 install --upgrade pip
-pip3 install Adafruit-Blinka
+pip3 install Adafruit-Blinka adafruit-circuitpython-vl53l0x pyserial RPi.GPIO
 
-# Install the Adafruit library for the VL53L0X sensor
-echo "Installing the Adafruit library for VL53L0X..."
-pip3 install adafruit-circuitpython-vl53l0x
+# Install ROS 2 Humble Hawksbill
+echo "Setting up ROS 2 Humble Hawksbill environment..."
+# Add the ROS 2 apt repository
+apt update && apt install -y curl gnupg2 lsb-release
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | apt-key add -
+echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/ros2.list
 
-# Install PySerial for UART communication
-echo "Installing PySerial for UART communication..."
-pip3 install pyserial
+# Install ROS 2 Humble Hawksbill
+apt update
+apt install -y ros-humble-desktop
 
-echo "Setup complete!"
+# Initialize rosdep
+rosdep init
+rosdep update
 
-# Reminder to reboot
-echo "Please reboot your Raspberry Pi to ensure all changes take effect."
+# Environment setup
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# Install additional ROS 2 utilities
+apt install -y python3-colcon-common-extensions python3-rosdep
+
+# Update pip and setuptools
+pip3 install -U pip setuptools
+
+echo "Setup complete! Please restart."
