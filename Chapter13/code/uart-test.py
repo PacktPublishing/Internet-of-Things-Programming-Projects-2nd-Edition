5b1743d5460e56d37c1da1e4c128b8fb857bc870
RaspberryPiEdge <raspberrypi.edge@gmail.com>
2024-03-17 19:42:23 -0400

    Update uart-test.py

--- Chapter13/code/uart-test.py
+++ Chapter13/code/uart-test.py
@@ -1,5 +1,9 @@
 import serial
 import time
+import subprocess
+
+# Set permissions on /dev/serial0
+subprocess.run(['sudo', 'chmod', 'a+rw', '/dev/serial0'], check=True)
 
 ser = serial.Serial('/dev/serial0', 115200, timeout=1)
 
